586e25235f470e1ef6203f0963b372fe2b2fbe7f
Earl Ashby <earl@p4demo.com>
2001-02-04 01:00:55 -0500

    Comments for jam's scan.h.
    [git-p4: depot-paths = "//depot/": change = 180]

--- Jam/MAIN/src/scan.h
+++ Jam/MAIN/src/scan.h
@@ -6,10 +6,24 @@
 
 /*
  * scan.h - the jam yacc scanner
+ *
+ * External functions:
+ *
+ *	yyerror( char *s ) - print a parsing error message
+ *	yyfparse( char *s ) - tell the scanner to include file s
+ *	yylex() - parse the next token, returning its type
+ *	yymode() - adjust lexicon of scanner
+ *	yyparse() - declaration for yacc parser
+ *
+ * The yymode() function is for the parser to adjust the lexicon of the
+ * scanner.  Aside from normal keyword scanning, there is a mode to
+ * handle action strings (look only for the closing }) and a mode to 
+ * ignore most keywords when looking for a punctuation keyword.  This 
+ * allows non-punctuation keywords to be used in lists without quoting.
  */
 
 /*
- * needed by parser, scanner
+ * YYSTYPE - value of a lexical token
  */
 
 # define YYSTYPE YYSYMBOL
@@ -27,7 +41,7 @@ extern YYSTYPE yylval;
 int yyerror();
 void yyfparse();
 int yylex();
-void yymode();	/* adjust lexicon of scanner */
+void yymode();
 int yyparse();
 
 # define SCAN_NORMAL	0	/* normal parsing */
