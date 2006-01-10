f760d254884ba1247cf948b5acebaffab37f658d
John Wakeman <JWakeman@p4demo.com>
2006-01-10 03:01:08 -0500

    Updated the EEnvironment.java files and EVersion.java files to reflect the new development environment.
    [git-p4: depot-paths = "//depot/": change = 402]

--- Talkhouse/main-dev/com/walkerbros/common/EVersion.java
+++ Talkhouse/main-dev/com/walkerbros/common/EVersion.java
@@ -2,6 +2,14 @@
 
 package com.walkerbros.common.ejava;
 
+
+/*
+ * EVersion.java - constants that define access to environment elements.
+ *
+ * 10/31/03 (jwalker) - defined file for the Talkhouse application.
+ * 11/14/03 (jwalker) - file now available to each branch. Should be updated
+ *			upon the creation of each branch.
+ */
 public interface EVersion
 {
 	// Version number
