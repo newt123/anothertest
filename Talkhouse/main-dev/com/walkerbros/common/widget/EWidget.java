037fd8d7502326787a8bf92b214a57f5cff5ce9d
John Wakeman <JWakeman@p4demo.com>
2006-01-10 03:02:59 -0500

    Added comments to clarify the use of default constructors.
    [git-p4: depot-paths = "//depot/": change = 403]

--- Talkhouse/main-dev/com/walkerbros/common/widget/EWidget.java
+++ Talkhouse/main-dev/com/walkerbros/common/widget/EWidget.java
@@ -10,7 +10,11 @@ public class EWidget
 	ENut  m_eNut 	= new ENut(ENut.E_NUT_STATE_TOTALLY_SCREWED_UP);
 	EBolt m_eBolt 	= new EBolt(EBolt.E_BOLT_STATE_TOTALLY_SCREWED_UP);
 
-
+	/*
+ 	 * Dummy construtor was introduced after a meeting with the standards committee.
+ 	 * It was decided that it was only good form to maintain these since there might be
+         * cases in the future were objected will be created and not initialized.
+         */
 	public EWidget() 
 	{
 	}
