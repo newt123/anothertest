88986512df1df486959932c0016145709e296869
Aruna_Gupta <Aruna_Gupta@p4demo.com>
2006-01-15 03:16:03 -0500

    Removed extraneous comments
    Jobs: job000016
    [git-p4: depot-paths = "//depot/": change = 408]

--- Talkhouse/main-dev/com/walkerbros/common/EEnvironment.java
+++ Talkhouse/main-dev/com/walkerbros/common/EEnvironment.java
@@ -4,9 +4,7 @@ package com.walkerbros.common;
 
 /*
  * EEnvironment.java - constants that define access to environment elements.
- *
- * 10/31/03 (jwalker) - defined file for the Talkhouse application.
- * 11/14/03 (jwalker) - added ini configuration support for the windows environment.
+
  */
 public interface EEnvironment
 {
