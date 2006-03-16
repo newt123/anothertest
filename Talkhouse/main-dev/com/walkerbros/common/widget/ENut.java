abdfa49c25343b1d8513dcc4933431400a169313
John Wakeman <JWakeman@p4demo.com>
2006-03-16 03:02:07 -0500

    merging changes from rel1.5 to main-dev
    [git-p4: depot-paths = "//depot/": change = 423]

--- Talkhouse/main-dev/com/walkerbros/common/widget/ENut.java
+++ Talkhouse/main-dev/com/walkerbros/common/widget/ENut.java
@@ -5,15 +5,15 @@ package com.walkerbros.common.widget;
 public class ENut
 {
 
-
-	public static final short E_NUT_STATE_SCREWED_OFF			= 0;
+// Constants for state of nut
+	public static final short E_NUT_STATE_SCREWED_OFF		= 0;
 	public static final short E_NUT_STATE_SCREWED_ON			= 1;
 	
 	// Configuration for a broken bolt.
 	public static final short E_NUT_STATE_TOTALLY_SCREWED_UP		= -1;
 
 	
-	public short m_currState = E_NUT_STATE_SCREWED_OFF;
+	public short m_currState = E_NUT_STATE_SCREWED_ON;
 	/*
  	 * Dummy construtor was introduced after a meeting with the standards committee.
  	 * It was decided that it was only good form to maintain these since there might be
